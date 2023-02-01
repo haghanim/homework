0d3cb8b727624a0f66ebd07364e64bc7480c5cc3
Mark Haghani <markjhaghani@gmail.com>
2023-01-31 21:05:53 -0500

    hw1 sub1

--- assignments/week1/model.py
+++ assignments/week1/model.py
@@ -7,24 +7,37 @@ class LinearRegression:
     b: float
 
     def __init__(self):
-        raise NotImplementedError()
+        self.w = []
+        self.b = 0
 
     def fit(self, X, y):
-        raise NotImplementedError()
+        # Closed form 
+
+        X_b = np.c_[np.ones((len(X), 1)), X]
+        self.w = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y)
+        self.b = self.w[0]
+        self.w = self.w[1:]
+
 
     def predict(self, X):
-        raise NotImplementedError()
+        X = np.c_[np.ones((len(X), 1)), X]
+        return X.dot(np.r_[self.b, self.w])
 
 
 class GradientDescentLinearRegression(LinearRegression):
     """
     A linear regression model that uses gradient descent to fit the model.
     """
-
+    
     def fit(
         self, X: np.ndarray, y: np.ndarray, lr: float = 0.01, epochs: int = 1000
     ) -> None:
-        raise NotImplementedError()
+        for _ in range(epochs): 
+            Y_pred = self.predict(X)
+            Dw = (-2/len(X)) * sum(X * (y - Y_pred))
+            Db = (-2/len(X)) * sum(y - Y_pred)
+            self.w = self.w - lr * Dw 
+            self.b = self.b - lr * Db 
 
     def predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -37,4 +50,6 @@ class GradientDescentLinearRegression(LinearRegression):
             np.ndarray: The predicted output.
 
         """
-        raise NotImplementedError()
+        X = np.array(X)
+        X = np.c_[np.ones((len(X), 1)), X]
+        return X.dot(np.r_[self.b, self.w])
