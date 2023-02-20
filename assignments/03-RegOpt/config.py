05b336e6d274c9b1644f30823f9a2908a61c17c8
Mark Haghani <markjhaghani@gmail.com>
2023-02-20 13:03:24 -0500

    adds simple lr algo

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -13,16 +13,23 @@ class CONFIG:
 
     batch_size = 64
     num_epochs = 10
-    initial_learning_rate = 0.001
+    initial_learning_rate = 0.01
     initial_weight_decay = 0
 
-    # ?? 128 batch_size -> higher test acc (just right, more is bad)??
+    step_size = 10
+    gamma = 0.1
+
+    # ?? 128 batch_size -> higher test acc (just right, more is worse)??
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
-        "step_size": 10,
-        "gamma": 0.1,
+        "gamma": gamma,
+        "num_epochs": num_epochs,
+        "initial_learning_rate": initial_learning_rate,
+        "step_size": step_size,
+        "batch_size": batch_size,
+        # 'initial_weight_decay': initial_weight_decay
     }
 
     optimizer_factory: Callable[
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -8,7 +8,17 @@ class CustomLRScheduler(_LRScheduler):
     A custom learning rate scheduler that extends the PyTorch _LRScheduler class.
     """
 
-    def __init__(self, optimizer, step_size, last_epoch=-1, gamma=0.1):
+    def __init__(
+        self,
+        optimizer,
+        step_size,
+        num_epochs,
+        initial_learning_rate,
+        initial_weight_decay,
+        batch_size,
+        last_epoch=-1,
+        gamma=0.1,
+    ):
         """
         Create a new scheduler.
 
@@ -19,8 +29,10 @@ class CustomLRScheduler(_LRScheduler):
         # ... Your Code Here ...
         self.step_size = step_size
         self.gamma = gamma
-
-        # batch_size,num_epochs,initial_learning_rate,last_epoch
+        self.batch_size = batch_size
+        self.num_epochs = num_epochs
+        self.initial_learning_rate = initial_learning_rate
+        self.initial_weight_decay = initial_weight_decay
 
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
         # leave alone
@@ -39,4 +51,23 @@ class CustomLRScheduler(_LRScheduler):
 
         # ... Your Code Here ...
         # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+        # return [i for i in self.base_lrs]
+
+        learning_rates = []
+        lr = self.initial_learning_rate
+
+        # DID NOT HELP
+        # # lr = lr / (self.batch_size ** 0.5)
+
+        for group in self.optimizer.param_groups:
+
+            # DID NOT HELP
+            # if (self.last_epoch == 0) or (self.last_epoch % self.step_size != 0):
+            #     lr = lr
+            # lr = (self.last_epoch // self.step_size) * lr
+
+            lr = self.gamma * lr
+            learning_rates.append(lr)
+
+        # Return the learning rate
+        return learning_rates
