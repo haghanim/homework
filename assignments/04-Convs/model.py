5daf8afe779edc87cd13d1dac0ed7eb56bbd6fa3
Mark Haghani <markjhaghani@gmail.com>
2023-03-15 17:51:29 -0400

    first cnn model

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -1,9 +1,35 @@
 import torch
+import torch.nn as nn
 
 
 class Model(torch.nn.Module):
     def __init__(self, num_channels: int, num_classes: int) -> None:
-        ...
+        super(Model, self).__init__()
+        self.num_channels = num_channels
+        self.num_classes = num_classes
+
+        self.conv1 = nn.Conv2d(in_channels=num_channels, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(64*8*8, 128) 
+        self.relu3 = nn.ReLU()
+        self.fc2 = nn.Linear(128, num_classes)
+
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        ...
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.pool2(x)
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.relu3(x)
+        x = self.fc2(x)
+
+        return x
