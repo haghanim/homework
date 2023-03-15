7102e3795a25a13e2124411b18b00209244d1420
Mark Haghani <markjhaghani@gmail.com>
2023-03-15 17:53:51 -0400

    first cnn model

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -8,17 +8,24 @@ class Model(torch.nn.Module):
         self.num_channels = num_channels
         self.num_classes = num_classes
 
-        self.conv1 = nn.Conv2d(in_channels=num_channels, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(
+            in_channels=num_channels,
+            out_channels=32,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+        )
         self.relu1 = nn.ReLU()
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(
+            in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1
+        )
         self.relu2 = nn.ReLU()
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(64*8*8, 128) 
+        self.fc1 = nn.Linear(64 * 8 * 8, 128)
         self.relu3 = nn.ReLU()
         self.fc2 = nn.Linear(128, num_classes)
-
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.conv1(x)
